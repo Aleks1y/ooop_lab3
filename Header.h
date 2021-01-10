769808e334ab26fc47794b0599bc43e2f21266a2
Aleks1y <74914340+Aleks1y@users.noreply.github.com>
2021-01-10 16:40:15 +0700

    Update Header.h

--- Header.h
+++ Header.h
@@ -7,13 +7,15 @@
 #define MISSED -1
 #define EMPTY_FIELD 0
 #define PLAYER_SHIP 1
+#define FIELD (isFirst ? field2 : field1) 
 
 using namespace std;
+using Coord = pair<int, int>;
 
 enum PlayerType
 {
     Random,
-    Optional,
+    Optimal,
     Interactive
 };
 
@@ -27,7 +29,8 @@ enum class ShipType
 
 class Player
 {
-public:
+    friend class Judge;
+protected:
     bool successOnLastStep = false;
     bool destroyShipOnLast = false;
     int field[10][10] = { { 0 } };
@@ -35,48 +38,58 @@ public:
     void SetShip(int x, int y, bool horizontal, ShipType type);
     void FillFieldRandom();
     void FillField();
-    virtual pair<int, int> nextStep() = 0;
-    static Player* createPlayer(PlayerType type);
+public:
+    void SetSuccessOnLastStep(bool val);
+    void SetDestroyShipOnLast(bool val);
+    static shared_ptr<Player> createPlayer(PlayerType type);
+    virtual Coord nextStep() = 0;
+    virtual ~Player() {};
 };
 
 class ConsolePlayer : public Player
 {
 public:
-    pair<int, int> nextStep() override;
+    Coord nextStep() override;
     ConsolePlayer();
 };
 
 class RandomPlayer : public Player
 {
 public:
-    pair<int, int> nextStep() override;
+    Coord nextStep() override;
     RandomPlayer();
 };
 
-class OptionalPlayer :public Player
+class OptimalPlayer :public Player
 {
 private:
     int vectorFire = 0;
-    vector<pair<int, int>> lastSuccessPoints;
-    pair<int, int>   lastPoint;
+    vector<Coord> lastSuccessPoints;
+    Coord   lastPoint;
     int diag0 = 0;
     int diag1 = 0;
     int diag2 = 0;
     int diag3 = 0;
-    pair<int, int> strategyFire();
+    Coord strategyFire();
 public:
-    pair<int, int> nextStep() override;
-    OptionalPlayer();
+    Coord nextStep() override;
+    OptimalPlayer();
 };
 
 class Judge
 {
-public:
+private:
+    int field1[10][10];
+    int field2[10][10];
     size_t player1destroyed = 0;
     size_t player2destroyed = 0;
-    bool isPlayerDead(Player player);
-    bool isGoodStep(int x, int y, Player* player);
-    int step(int x, int y, Player* player);
+public:
+    size_t GetPlayerDestroyed(bool isFirst);
+    void IncPlayerDestroyed(bool isFirst);
+    bool isGoodField(const int (&field)[10][10]);
+    bool isGoodStep(int x, int y, bool isFirst);
+    int step(int x, int y, bool isFirst);
+    Judge(shared_ptr<Player> player1, shared_ptr<Player> player2);
 };
 
 void execute(int rounds, PlayerType firstType, PlayerType secondType);

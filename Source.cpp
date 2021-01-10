dac0b7609ec25ab0ab56ec9eacb04874a40eb2e6
Aleks1y <74914340+Aleks1y@users.noreply.github.com>
2021-01-10 16:41:32 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -87,16 +87,26 @@ bool Player::isGoodShip(int x, int y, bool horizontal, ShipType type)
     return true;
 }
 
-Player* Player::createPlayer(PlayerType type)
+void Player::SetSuccessOnLastStep(bool val)
+{
+    successOnLastStep = val;
+}
+
+void Player::SetDestroyShipOnLast(bool val)
+{
+    destroyShipOnLast = val;
+}
+
+shared_ptr<Player> Player::createPlayer(PlayerType type)
 {
     switch (type)
     {
     case Random:
-        return new RandomPlayer();
-    case Optional:
-        return new OptionalPlayer();
+        return make_shared<RandomPlayer>();
+    case Optimal:
+        return make_shared<OptimalPlayer>();
     case Interactive:
-        return new ConsolePlayer();
+        return make_shared<ConsolePlayer>();
     }
 }
 void Player::FillFieldRandom()
@@ -158,7 +168,7 @@ void Player::FillField()
     {
         do
         {
-            cout << "Enter information about №" << i << "three decked ship (int x, int y, bool ishorizontal)";
+            cout << "Enter information about #" << i + 1 << " three decked ship (int x, int y, bool ishorizontal)";
             cin >> x >> y >> ishorizontal;
         } while (!isGoodShip(x - 1, y - 1, ishorizontal, ShipType::three));
         SetShip(x - 1, y - 1, ishorizontal, ShipType::three);
@@ -167,7 +177,7 @@ void Player::FillField()
     {
         do
         {
-            cout << "Enter information about  №" << i << "two decked ship (int x, int y, bool ishorizontal)";
+            cout << "Enter information about #" << i + 1 << " two decked ship (int x, int y, bool ishorizontal)";
             cin >> x >> y >> ishorizontal;
         } while (!isGoodShip(x - 1, y - 1, ishorizontal, ShipType::two));
         SetShip(x - 1, y - 1, ishorizontal, ShipType::two);
@@ -176,20 +186,20 @@ void Player::FillField()
     {
         do
         {
-            cout << "Enter information about  №" << i << "one decked ship (int x, int y, bool ishorizontal)";
+            cout << "Enter information about #" << i + 1 << " one decked ship (int x, int y, bool ishorizontal)";
             cin >> x >> y >> ishorizontal;
         } while (!isGoodShip(x - 1, y - 1, ishorizontal, ShipType::one));
         SetShip(x - 1, y - 1, ishorizontal, ShipType::one);
     }
 }
 
-pair<int, int> ConsolePlayer::nextStep()
+Coord ConsolePlayer::nextStep()
 {
     cout << "Enter int x int y" << endl;
 
     int x, y;
     cin >> x >> y;
-    return make_pair(x - 1, y - 1);
+    return { x - 1, y - 1 };
 }
 
 ConsolePlayer::ConsolePlayer()
@@ -208,12 +218,12 @@ ConsolePlayer::ConsolePlayer()
         FillField();
 }
 
-pair<int, int> RandomPlayer::nextStep()
+Coord RandomPlayer::nextStep()
 {
     int x = rand() % 10;
     int y = rand() % 10;
 
-    return make_pair(x, y);
+    return { x, y };
 }
 
 RandomPlayer::RandomPlayer()
@@ -221,40 +231,40 @@ RandomPlayer::RandomPlayer()
     FillFieldRandom();
 }
 
-pair<int, int> OptionalPlayer::strategyFire()
+Coord OptimalPlayer::strategyFire()
 {
     if (diag0 < 10)
     {
-        lastPoint = make_pair(diag0, diag0);
+        lastPoint = { diag0, diag0 };
         diag0++;
         return lastPoint;
     }
     else if (diag1 < 10)
     {
-        lastPoint = make_pair(9 - diag1, diag1);
+        lastPoint = { 9 - diag1, diag1 };
         diag1++;
         return lastPoint;
     }
     else if (diag2 < 10)
     {
-        lastPoint = make_pair(4, diag2);
+        lastPoint = { 4, diag2 };
         diag2++;
         return lastPoint;
     }
     else if (diag3 < 10)
     {
-        lastPoint = make_pair(diag3, 5);
+        lastPoint = { diag3, 5 };
         diag3++;
         return lastPoint;
     }
     else
     {
-        lastPoint = make_pair(rand() % 10, rand() % 10);
+        lastPoint = { rand() % 10, rand() % 10 };
         return lastPoint;
     }
 }
 
-pair<int, int> OptionalPlayer::nextStep()
+Coord OptimalPlayer::nextStep()
 {
     if (!successOnLastStep && lastSuccessPoints.empty())
         return strategyFire();
@@ -278,85 +288,148 @@ pair<int, int> OptionalPlayer::nextStep()
     switch (vectorFire)
     {
     case 0:
-        lastPoint = make_pair(lastSuccessPoints[lastSuccessPoints.size() - 1].first + 1, lastSuccessPoints[lastSuccessPoints.size() - 1].second);
+        lastPoint = { lastSuccessPoints[lastSuccessPoints.size() - 1].first + 1, lastSuccessPoints[lastSuccessPoints.size() - 1].second };
         break;
     case 1:
-        lastPoint = make_pair(lastSuccessPoints[lastSuccessPoints.size() - 1].first - 1, lastSuccessPoints[lastSuccessPoints.size() - 1].second);
+        lastPoint = {lastSuccessPoints[lastSuccessPoints.size() - 1].first - 1, lastSuccessPoints[lastSuccessPoints.size() - 1].second};
         break;
     case 2:
-        lastPoint = make_pair(lastSuccessPoints[lastSuccessPoints.size() - 1].first, lastSuccessPoints[lastSuccessPoints.size() - 1].second + 1);
+        lastPoint = { lastSuccessPoints[lastSuccessPoints.size() - 1].first, lastSuccessPoints[lastSuccessPoints.size() - 1].second + 1 };
         break;
     case 3:
-        lastPoint = make_pair(lastSuccessPoints[lastSuccessPoints.size() - 1].first, lastSuccessPoints[lastSuccessPoints.size() - 1].second - 1);
+        lastPoint = { lastSuccessPoints[lastSuccessPoints.size() - 1].first, lastSuccessPoints[lastSuccessPoints.size() - 1].second - 1 };
         break;
     }
 
     return lastPoint;
 }
 
-OptionalPlayer::OptionalPlayer()
+OptimalPlayer::OptimalPlayer()
 {
     FillFieldRandom();
 }
 
-bool Judge::isGoodStep(int x, int y, Player* player)
+
+size_t Judge::GetPlayerDestroyed(bool isFirst)
+{
+    return isFirst ? player1destroyed : player2destroyed;
+}
+
+void Judge::IncPlayerDestroyed(bool isFirst)
+{
+    isFirst ? player1destroyed++ : player2destroyed++;
+}
+
+bool Judge::isGoodField(const int (&field)[10][10])
+{
+    size_t emptySquares = 0,
+        countOne = 0,
+        countTwo = 0,
+        countThree = 0,
+        countFour = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (field[i][j] == PLAYER_SHIP)
+            {
+                if (!(i - 1 >= 0 && field[i - 1][j] == PLAYER_SHIP) && !(j - 1 >= 0 && field[i][j - 1] == PLAYER_SHIP))
+                {
+                    size_t length = 1,
+                        vectorY = 0,
+                        vectorX = 0;
+                    if (i + 1 < 10 && field[i + 1][j] == PLAYER_SHIP)
+                        vectorY = 1;
+                    else
+                        vectorX = 1;
+                    while (i + vectorY < 10 && j + vectorX < 10 && field[i + vectorY][j + vectorX] == PLAYER_SHIP)
+                    {
+                        length++;
+                        vectorY ? vectorY++ : vectorX++;
+                    }
+
+                    if (length == 1)
+                        countOne++;
+                    else if (length == 2)
+                        countTwo++;
+                    else if (length == 3)
+                        countThree++;
+                    else if (length == 4)
+                        countFour++;
+                    else
+                        return false;
+                }
+            }
+            else
+            {
+                emptySquares++;
+            }
+        }
+    }
+    if (countOne == 4 && countTwo == 3 && countThree == 2 && countFour == 1 && emptySquares == (100 - 4 - 3 * 2 - 2 * 3 - 4))
+        return true;
+
+    return false;
+}
+
+bool Judge::isGoodStep(int x, int y, bool isFirst)
 {
     if (x < 0 || y < 0 || x > 9 || y > 9)
         return false;
     
 
-    if(player->field[y][x] < 0 || player->field[y][x] > 1)
+    if(FIELD[y][x] < 0 || FIELD[y][x] > 1)
         return false;
 
     return true;
 }
 
-int Judge::step(int x, int y, Player* player)
+int Judge::step(int x, int y, bool isFirst)
 {
-    if (player->field[y][x] == EMPTY_FIELD)
+    if (FIELD[y][x] == EMPTY_FIELD)
     {
-        player->field[y][x] = MISSED;
+        FIELD[y][x] = MISSED;
         return MISSED;
     }
-    if (player->field[y][x] == PLAYER_SHIP)
+    if (FIELD[y][x] == PLAYER_SHIP)
     {
         
-        player->field[y][x] = DAMAGED;
+        FIELD[y][x] = DAMAGED;
 
-        for (int i = 1; y - i > -1 && player->field[y - i][x] > 0; i++)
+        for (int i = 1; y - i > -1 && (isFirst ? field2 : field1)[y - i][x] > 0; i++)
         {
-            if (player->field[y - i][x] == PLAYER_SHIP)
+            if (FIELD[y - i][x] == PLAYER_SHIP)
                 return DAMAGED;
         }
 
-        for (int i = 1; y + i < 10 && player->field[y + i][x] > 0; i++)
+        for (int i = 1; y + i < 10 && FIELD[y + i][x] > 0; i++)
         {
-            if (player->field[y + i][x] == PLAYER_SHIP)
+            if (FIELD[y + i][x] == PLAYER_SHIP)
                 return DAMAGED;
         }
 
-        for (int i = 1; x - i > -1 && player->field[y][x - i] > 0; i++)
+        for (int i = 1; x - i > -1 && FIELD[y][x - i] > 0; i++)
         {
-            if (player->field[y][x - i] == PLAYER_SHIP)
+            if (FIELD[y][x - i] == PLAYER_SHIP)
                 return DAMAGED;
         }
 
-        for (int i = 1; x + i < 10 && player->field[y][x + i] > 0; i++)
+        for (int i = 1; x + i < 10 && FIELD[y][x + i] > 0; i++)
         {
-            if (player->field[y][x + i] == PLAYER_SHIP)
+            if (FIELD[y][x + i] == PLAYER_SHIP)
                 return DAMAGED;
         }
 
         int vectorY = 0,
             vectorX = 0;
 
-        if (y - 1 >= 0 && player->field[y - 1][x] == DAMAGED)
+        if (y - 1 >= 0 && FIELD[y - 1][x] == DAMAGED)
             vectorY = -1;
-        else if (y + 1 < 10 && player->field[y + 1][x] == DAMAGED)
+        else if (y + 1 < 10 && FIELD[y + 1][x] == DAMAGED)
             vectorY = 1;
-        else if (x - 1 >= 0 && player->field[y][x - 1] == DAMAGED)
+        else if (x - 1 >= 0 && FIELD[y][x - 1] == DAMAGED)
             vectorX = -1;
-        else if (x + 1 < 10 && player->field[y][x + 1] == DAMAGED)
+        else if (x + 1 < 10 && FIELD[y][x + 1] == DAMAGED)
             vectorX = 1;
         
         do 
@@ -367,19 +440,34 @@ int Judge::step(int x, int y, Player* player)
                 {
                     for (int j = -1; j < 2; j++)
                     {
-                        if (x + j < 10 && x + j >= 0 && player->field[y + i][x + j] == EMPTY_FIELD)
+                        if (x + j < 10 && x + j >= 0 && FIELD[y + i][x + j] == EMPTY_FIELD)
                         {
-                            player->field[y + i][x + j] = MISSED;
+                            FIELD[y + i][x + j] = MISSED;
                         }
                     }
                 }
             }
             y += vectorY;
             x += vectorX;
-        } while ((vectorY != 0 || vectorX != 0) && y < 10 && x < 10 && y >= 0 && x >= 0 && player->field[y][x] == DAMAGED);
+        } while ((vectorY != 0 || vectorX != 0) && y < 10 && x < 10 && y >= 0 && x >= 0 && FIELD[y][x] == DAMAGED);
 
         return DESTROYED;
     }
+}
+
+Judge::Judge(shared_ptr<Player> player1, shared_ptr<Player> player2)
+{
+    for (size_t i = 0; i < 10; i++)
+    {
+        for (size_t j = 0; j < 10; j++)
+        {
+            field1[i][j] = player1->field[i][j];
+            field2[i][j] = player2->field[i][j];
+        }
+    }
+
+    if (!isGoodField(field1) || !isGoodField(field2))
+        throw("Bad field");
 }
 
 void execute(int rounds, PlayerType firstType, PlayerType secondType)
@@ -389,49 +477,49 @@ void execute(int rounds, PlayerType firstType, PlayerType secondType)
     int scoreSecond = 0;
     for (size_t i = 0; i < rounds; i++)
     {
-        Player* first = Player::createPlayer(firstType);
-        Player* second = Player::createPlayer(secondType);
+        shared_ptr<Player> first = Player::createPlayer(firstType);
+        shared_ptr<Player> second = Player::createPlayer(secondType);
 
-        Judge judge;
+        Judge judge(first, second);
 
         bool motion = 1;
-        while (judge.player1destroyed != 10 && judge.player2destroyed != 10)
+        while (judge.GetPlayerDestroyed(1) != 10 && judge.GetPlayerDestroyed(0) != 10)
         {
-            pair<int, int> step;
+            Coord step;
             do
             {
                 step = (motion ? first : second)->nextStep();
-                if (!judge.isGoodStep(step.first, step.second, (motion ? second : first)))
+                if (!judge.isGoodStep(step.first, step.second, motion))
                 {
-                    (motion ? first : second)->successOnLastStep = 0;
+                    (motion ? first : second)->SetSuccessOnLastStep(0);
                     continue;
                 }
                 break;
             } while (true);
 
-            switch (judge.step(step.first, step.second, (motion ? second : first)))
+            switch (judge.step(step.first, step.second, motion))
             {
             case MISSED:
-                (motion ? first : second)->successOnLastStep = 0;
-                (motion ? first : second)->destroyShipOnLast = 0;
+                (motion ? first : second)->SetSuccessOnLastStep(0);
+                (motion ? first : second)->SetDestroyShipOnLast(0);
                 cout << (motion ? "First player missed" : "Second player missed") << endl;
                 motion ? motion = false : motion = true;
                 break;
             case DAMAGED:
-                (motion ? first : second)->successOnLastStep = 1;
-                (motion ? first : second)->destroyShipOnLast = 0;
+                (motion ? first : second)->SetSuccessOnLastStep(1);
+                (motion ? first : second)->SetDestroyShipOnLast(0);
                 cout << (motion ? "First player damaged" : "Second player damaged") << endl;
                 break;
             case DESTROYED:
-                (motion ? first : second)->successOnLastStep = 1;
-                (motion ? first : second)->destroyShipOnLast = 1;
-                motion ? judge.player1destroyed++ : judge.player2destroyed++;
+                (motion ? first : second)->SetSuccessOnLastStep(1);
+                (motion ? first : second)->SetDestroyShipOnLast(1);
+                judge.IncPlayerDestroyed(motion);
                 cout << (motion ? "First player destroyed" : "Second player destroyed") << endl;
                 break;
             }
         }
 
-        if (judge.player1destroyed == 10)
+        if (judge.GetPlayerDestroyed(1) == 10)
             scoreFirst++;
         else
             scoreSecond++;
